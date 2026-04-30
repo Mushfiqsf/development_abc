57aaa8847a24c8183a5a7eaaa07d3edbf303b67e
Mushfiq Fahim <mushfiq.fahims@gmail.com>
2026-04-30 09:11:20 +0600

    Add files via upload

---
+++ Assingment_1_ShopApp.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Assingment1_ShopApp {
+    public static void main(String[] args) {
+
+        System.out.println("--- Welcome to the Mushfiq Shop--");
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter Product Name: ");
+        String Product_Name = sc.nextLine();
+
+        System.out.print("Enter unit Price: ");
+        double Unit_Price = sc.nextDouble();
+
+        System.out.print("Enter Quantity: ");
+        int Quantity = sc.nextInt();
+
+        double Subtotal = Unit_Price * Quantity;
+        double Discount = Subtotal * (.12);
+        double total = Subtotal - Discount;
+        int final_ = (int) Math.floor(total);
+
+        System.out.println("--- Final Receipt---");
+        System.out.println("Item : " + Product_Name);
+        System.out.println("Subtotal : $" + Subtotal);
+        System.out.println("Discount Applied :  $" + Discount);
+        System.out.println("Total (Rounded Down) :  $" + final_);
+        System.out.println("-------------------------------");
+    }
+}
