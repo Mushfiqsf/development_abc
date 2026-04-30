7829fc4e2a8b5255776852b8198a40bfacf9a28b
Mushfiq Fahim <mushfiq.fahims@gmail.com>
2026-04-30 09:05:02 +0600

    Refactor variable name for final total calculation

--- Assingment1_ShopApp.java
+++ Assingment1_ShopApp.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 
 public class Assingment1_ShopApp {
     public static void main(String[] args) {
+        
         System.out.println("--- Welcome to the Mushfiq Shop--");
         Scanner sc = new Scanner(System.in);
 
@@ -13,17 +14,17 @@ public class Assingment1_ShopApp {
 
         System.out.print("Enter Quantity: ");
         int Quantity = sc.nextInt();
+        
         double Subtotal = Unit_Price * Quantity;
         double Discount = Subtotal * (.12);
         double total = Subtotal - Discount;
-        int rt = (int) Math.floor(total);
+        int final_ = (int) Math.floor(total);
 
         System.out.println("--- Final Receipt---");
-
         System.out.println("Item : " + Product_Name);
         System.out.println("Subtotal : $" + Subtotal);
         System.out.println("Discount Applied :  $" + Discount);
-        System.out.println("Total (Rounded Down) :  $" + rt);
+        System.out.println("Total (Rounded Down) :  $" + final_);
         System.out.println("-------------------------------");
     }
 }
